6dafe7d67fa8e8d4bde2793f04dee158f1b1b546
bagusdewantoro <bagusdewantoro86@gmail.com>
2023-02-02 13:06:16 +0700

    CardPicker Console App

---
+++ CardPicker/CardPicker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardPickerFile
+{
+    class CardPicker
+    {
+        static Random random = new Random(); // generate random numbers
+
+        // Method 1: PickSomeCards()
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i=0; i<numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        // Method 2: RandomSuit()
+        private static string RandomSuit()
+        {
+            //throw new NotImplementedException();
+            int value = random.Next(1, 5); // get a random number from 1 to 4
+            if (value == 1) return "Spades";
+            if (value == 2) return "Hearts";
+            if (value == 3) return "Clubs";
+            return "Diamonds"; // 4 is not 1 or 2 or 3
+        }
+
+        // Method 3: RandomValue()
+        private static string RandomValue()
+        {
+            //throw new NotImplementedException();
+            int value = random.Next(1, 14);
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
+        }
+    }
+}
---
+++ CardPicker/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CardPickerFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of cards to pick: ");
+            string inputNumber = Console.ReadLine(); // user input
+
+            // // ===== CARA PERTAMA: (sukses kalo input type: int, tapi kalo input non int, gagal) =====
+            // int valueFromUser = int.Parse(inputNumber); // parse input from string to int
+            // foreach (string card in CardPicker.PickSomeCards(valueFromUser))
+            //   {
+            //     Console.WriteLine(card);
+            //   }
+            // // ==========
+
+            // ===== CARA KEDUA: (ada handling jika input type: non int) =====
+            if (int.TryParse(inputNumber, out int valueFromUser))  // parse string input to int, but, check first the type of input
+            {
+                foreach (string card in CardPicker.PickSomeCards(valueFromUser))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number");
+            }
+            // ==========
+        }
+    }
+}
