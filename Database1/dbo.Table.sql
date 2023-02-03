629e9adafc626ac64d2458a394b17799cb4adf9d
bagusdewantoro <bagusdewantoro86@gmail.com>
2023-02-03 17:46:32 +0700

    Sql Database in progress

---
+++ Database1/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tbl_employees]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [first_name] NVARCHAR(50) NULL, 
+    [last_name] NVARCHAR(50) NULL, 
+    [job_title] NVARCHAR(50) NULL,
+	[department] NVARCHAR (50) NULL
+)
